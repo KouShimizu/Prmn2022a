e26d9090696afc620561f1371d8cae68f76597fb
KoShimizu <b2211260@photon.chitose.ac.jp>
2022-10-31 21:37:34 +0900

    課題1-5

--- prmn2022a/src/lecture01/Main.java
+++ prmn2022a/src/lecture01/Main.java
@@ -25,6 +25,42 @@ public class Main {
         System.out.println("sum = " + sum);
 
         //課題1-5
-
+        int[] score = {41, 85, 72, 38, 80};
+        result(score);
+        System.out.println("最高点:" + max(score) + "点");
+        System.out.println("最低点:" + min(score) + "点");
+        System.out.println("平均点:" + average(score) + "点");
     }
+    static void result(int score[]){
+        for(int i = 0; i < score.length; i++){
+            System.out.print(i + "番 " + score[i] + "点 ");
+            if(0 <= score[i] && score[i] < 60) System.out.println("不可");
+            else if(60 <= score[i] && score[i] < 70) System.out.println("可");
+            else if(70 <= score[i] && score[i] < 80) System.out.println("良");
+            else if(80 <= score[i] && score[i] < 90) System.out.println("優");
+            else if(90 <= score[i] && score[i] <= 100) System.out.println("秀");
+            else System.out.println("error");
+        }
+    }
+    static int min(int score[]){
+        int x = 100;
+        for (int item : score) {
+            if (x > item) x = item;
+        }
+        return x;
+    }
+    static int max(int score[]){
+        int x = 0;
+        for (int item : score) {
+            if (x < item) x = item;
+        }
+        return x;
+    }
+    static double average(int score[]){
+        float x = 0;
+        for (float item : score) x += item;
+        x /= score.length;
+        return x;
+    }
+
 }
