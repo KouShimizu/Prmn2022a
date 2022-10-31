02b6b3c13255663b20abd99667351b8d5c527f35
KoShimizu <b2211260@photon.chitose.ac.jp>
2022-10-31 21:17:19 +0900

    課題1-4

--- prmn2022a/src/lecture01/Main.java
+++ prmn2022a/src/lecture01/Main.java
@@ -13,5 +13,18 @@ public class Main {
         int age = 20;
         if(age < 20) System.out.println("I am " + age + " years old so I cannot drink liquor.");
         else System.out.println("I am " + age + " years old so I can drink liquor.");
+
+        //課題1-4
+        int[] array = new int[100];
+        int sum = 0;
+        int i = 0;
+        for(i = 0; i < 100; i++) {
+            array[i] = i + 1;
+            if (array[i] % 2 == 0) sum = sum + array[i];
+        }
+        System.out.println("sum = " + sum);
+
+        //課題1-5
+
     }
 }
