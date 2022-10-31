713f29810400ae587208c9294f8c81e5a876e318
KoShimizu <b2211260@photon.chitose.ac.jp>
2022-10-31 20:49:12 +0900

    課題1-2

--- prmn2022a/src/lecture01/Main.java
+++ prmn2022a/src/lecture01/Main.java
@@ -2,6 +2,7 @@ package lecture01;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("B2211260 Ko Shimizu");
+        int studentNumber = 2211260;
+        System.out.println("B" + studentNumber + " Ko Shimizu");
     }
 }
