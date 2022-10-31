17c061cbc5b44a27b8b6217dcb5a4c3fd75ad9fc
KoShimizu <b2211260@photon.chitose.ac.jp>
2022-10-31 20:52:22 +0900

    課題1-3

--- prmn2022a/src/lecture01/Main.java
+++ prmn2022a/src/lecture01/Main.java
@@ -2,9 +2,16 @@ package lecture01;
 
 public class Main {
     public static void main(String[] args) {
+        //課題1-1
         System.out.println("B2211260 Ko Shimizu");
 
+        //課題1-2
         int studentNumber = 2211260;
         System.out.println("B" + studentNumber + " Ko Shimizu");
+
+        //課題1-3
+        int age = 20;
+        if(age < 20) System.out.println("I am " + age + " years old so I cannot drink liquor.");
+        else System.out.println("I am " + age + " years old so I can drink liquor.");
     }
 }
